d548db6cbae801f33ff22d88db5a225ba2181edc
Aslan <tynyshtykaslan06@gmail.com>
2026-01-14 14:04:18 +0500

    eureka for services

--- services/config-server/src/main/java/com/project/configserver/ConfigServerApplication.java
+++ services/config-server/src/main/java/com/project/configserver/ConfigServerApplication.java
@@ -2,8 +2,10 @@ package com.project.configserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class ConfigServerApplication {
 
 	public static void main(String[] args) {
