56cddb5e497a054e6940fd9126afd6c812ea4bd3
Aslan <tynyshtykaslan06@gmail.com>
2026-01-16 22:16:51 +0500

    divided eureka and conf-server fixed errors

--- services/config-server/src/main/java/com/project/configserver/ConfigServerApplication.java
+++ services/config-server/src/main/java/com/project/configserver/ConfigServerApplication.java
@@ -3,10 +3,8 @@ package com.project.configserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableEurekaServer
 @EnableConfigServer
 public class ConfigServerApplication {
 
